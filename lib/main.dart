b0f6ddff291155476f52539ad5b884a074f492f4
foolish-pine <matsuda.takuya41@gmail.com>
2024-10-21 12:34:35 +0900

    feat: add feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +33,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  List<String> _items = [];
+  String _errorMessage = '';
+
+  Future<void> loadZipCode(String zipCode) async {
+    setState(() {
+      _errorMessage = 'APIレスポンス待ち';
+    });
+
+    final response = await http.get(
+        Uri.parse('https://zipcloud.ibsnet.co.jp/api/search?zipcode=$zipCode'));
+
+    if (response.statusCode != 200) {
+      setState(() {
+        _errorMessage = '郵便番号の取得に失敗しました';
+      });
+      return;
+    }
+
+    final body = json.decode(response.body) as Map<String, dynamic>;
+    final results = (body['results'] ?? []) as List<dynamic>;
+
+    if (results.isEmpty) {
+      setState(() {
+        _errorMessage = 'そのような郵便番号の住所はありません';
+      });
+    } else {
+      setState(() {
+        _errorMessage = '';
+        _items = results
+            .map((result) =>
+                "${result["address1"]}${result["address2"]}${result["address3"]}")
+            .toList(growable: false);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +77,33 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              if (value.isNotEmpty) loadZipCode(value);
+            },
+          ),
+          _errorMessage.isNotEmpty
+              ? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    _errorMessage,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: _items.length,
+                    itemBuilder: (context, index) => ListTile(
+                      title: Text(_items[index]),
+                    ),
+                  ),
+                ),
+        ],
+      ),
     );
   }
 }
